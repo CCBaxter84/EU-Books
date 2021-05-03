371e918d22f20c773ca7cc230df6c79fb238fee9
CCBaxter84 <troyceclark@gmail.com>
2021-05-03 16:48:51 -0600

    Fixed false pos error msg on canceling book post

--- public/javascripts/stars.js
+++ public/javascripts/stars.js
@@ -2,7 +2,9 @@
 const numStars = 100;
 
 // Loop over each star and randomly place it on the screen
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", memoizeRenderStars);
+
+function callback() {
   for (let i = 0; i < numStars; i++) {
     const star = document.createElement("div");
     star.className = "star";
@@ -11,13 +13,30 @@ document.addEventListener("DOMContentLoaded", function() {
     star.style.left = xy[1] + "px";
     document.body.append(star);
   }
-});
+}
 
 // Helper function for getting a random position
+
 function getRandomPosition() {
   const y = window.innerWidth;
   const x = window.innerHeight;
   const randomX = Math.floor(Math.random() * x);
   const randomY = Math.floor(Math.random() * y);
   return [randomX, randomY];
+}
+
+function memoizeRenderStars() {
+  for (let i = 0; i < numStars; i++) {
+    const star = document.createElement("div");
+    star.className = "star";
+    console.log(typeof localStorage.getItem(i));
+    if (!localStorage.getItem(i)) {
+      const xy = getRandomPosition();
+      localStorage.setItem(i, xy);
+    }
+    const xy = localStorage.getItem(i).split(",");
+    star.style.top = xy[0] + "px";
+    star.style.left = xy[1] + "px";
+    document.body.append(star);
+  }
 }
