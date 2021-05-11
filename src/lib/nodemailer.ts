3a48aa45b3827641e3c53702961f4c55105be7a9
CCBaxter84 <troyceclark@gmail.com>
2021-05-11 17:46:21 -0600

    Implemented gmail oauth and tested password reset -- it works

--- src/lib/nodemailer.ts
+++ src/lib/nodemailer.ts
@@ -4,7 +4,11 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 // Import dependencies
-import nodemailer from "nodemailer";
+import nodemailer, { createTransport } from "nodemailer";
+import * as SMTPTransport from "nodemailer/lib/smtp-transport";
+import { google } from "googleapis";
+import { access } from "node:fs";
+const OAuth2 = google.auth.OAuth2;
 
 // Declare interfaces
 interface IProps {
@@ -13,20 +17,39 @@ interface IProps {
   text: string;
 }
 
-// Declare & export function to send password reset email
-export const sendEmail = async function(props: IProps) {
-  // Destructure props
-  const { to, subject, text } = props;
-  console.log("host: ", process.env.EMAIL_HOST);
+const createTransporter = async function() {
+  // Create OAuth client
+  const oauth2Client = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+  oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((error, token) => {
+      if (error) {
+        reject();
+      } else {
+        resolve(token);
+      }
+    });
+  });
+
   // Create nodemailer transporter
   const transporter = nodemailer.createTransport({
     service: process.env.EMAIL_SERVICE,
     auth: {
+      type: "OAuth2",
       user: process.env.EMAIL_ADDRESS,
-      pass: process.env.EMAIL_PASSWORD
-    },
-    secure: true
-  });
+      accessToken: accessToken,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN
+    }
+  } as SMTPTransport.Options);
   transporter.verify((error, success) => {
     if (error) {
       console.log(error);
@@ -34,9 +57,15 @@ export const sendEmail = async function(props: IProps) {
       console.log("Hold on to your butts...");
     }
   });
+  return transporter;
+};
 
-  // Send the email
+// Declare & export function to send password reset email
+export const sendEmail = async function(props: IProps) {
+  // Destructure props
+  const { to, subject, text } = props;
   try {
+    let transporter = await createTransporter();
     await transporter.sendMail({
       from: `${process.env.EMAIL_ADDRESS}`,
       to,
