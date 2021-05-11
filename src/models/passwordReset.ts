553bd1c67287c61db38dbf0f8dd5ba5141e311b2
CCBaxter84 <troyceclark@gmail.com>
2021-05-11 10:17:29 -0600

    Created PasswordReset model

---
+++ src/models/passwordReset.ts
@@ -0,0 +1,28 @@
+// Import dependencies
+import { Schema, Document, model, Model } from "mongoose";
+
+// Define and export interface derived from mongoose Document
+export interface IPasswordReset extends Document {
+  user: Schema.Types.ObjectId,
+  token: string
+}
+
+// Define schema
+const passwordResetSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+// Define and export Book model based on schema
+const PasswordReset: Model<IPasswordReset> = model("User", passwordResetSchema);
+
+export default PasswordReset;
