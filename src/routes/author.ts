7eefe7609eafac5c29d264f31775847a2ac5077e
CCBaxter84 <troyceclark@gmail.com>
2021-05-14 20:06:27 -0600

    Updated author router with renderError fn

--- src/routes/author.ts
+++ src/routes/author.ts
@@ -2,10 +2,10 @@
 import { Router, Request, Response } from "express";
 import Author, { IAuthor } from "../models/author";
 import Book from "../models/book";
-import { LeanDocument } from "mongoose";
 import { isAuthenticated } from "../lib/middleware/auth";
 import { authorFormChecker } from "../lib/middleware/forms";
 import { DB_LOOKUP_ERR, BAD_REQ_ERR, PAGE_ERR, AUTHOR_EXISTS_ERR, AUTHOR_HAS_BOOKS_ERR, INVALID_AUTHOR_ERR } from "../lib/global-constants";
+import { renderError } from "../lib/error-utils";
 
 // Define interfaces
 interface IParams {
@@ -30,9 +30,8 @@ router.get("/", async (req: Request, res: Response) => {
   // Set var to conditionally render based on auth status
   const isAuth = req.user ? true : false;
   // Initialize authors var to pass to view
-  let authors: LeanDocument<IAuthor>[] | null = null;
   try {
-    authors = await Author.find(searchOptions).lean();
+    const authors = await Author.find(searchOptions).lean();
     res.render("authors/index", {
       authors,
       isAuth,
@@ -40,18 +39,7 @@ router.get("/", async (req: Request, res: Response) => {
       searchOptions: req.query
     });
   } catch {
-    if (authors == null) {
-      res.render("authors/index", {
-        searchOptions: req.query,
-        isAuth,
-        error: DB_LOOKUP_ERR
-      });
-    } else {
-      res.render("/", {
-        isAuth,
-        error: PAGE_ERR
-      });
-    }
+    renderError("server-err", res, isAuth);
   }
 });
 
@@ -59,28 +47,23 @@ router.get("/", async (req: Request, res: Response) => {
 // @desc    Render form for adding a new author to screen
 // @access  Private
 router.get("/new", isAuthenticated, (req: Request, res: Response) => {
-  res.render("authors/new", { author: new Author(), isAuth: true, csrfToken: req.csrfToken() });
+  const isAuth = req.user ? true : false;
+  res.render("authors/new", { author: new Author(), isAuth, csrfToken: req.csrfToken() });
 });
 
 // @route   POST /authors
 // @desc    Add a new author to the database
 // @access  Private
 router.post("/", isAuthenticated, authorFormChecker, async (req: Request, res: Response) => {
-  let author = new Author({
+  const isAuth = req.user ? true : false;
+  const newAuthor = new Author({
     name: req.body.name
   });
   try {
-    const newAuthor = await author.save();
-    res.redirect(`/authors/${newAuthor._id}`);
-  } catch(err) {
-    let error = AUTHOR_EXISTS_ERR;
-    if (err.message != AUTHOR_EXISTS_ERR) {
-      error = "Error saving author";
-    }
-    res.render("authors/new", {
-      error,
-      isAuth: true
-    });
+    const savedAuthor = await newAuthor.save();
+    res.redirect(`/authors/${savedAuthor._id}`);
+  } catch {
+    renderError("server-err", res, isAuth);
   }
 });
 
@@ -98,8 +81,8 @@ router.get("/:id", async (req: Request, res: Response) => {
     res.render("authors/show", { author, books, isAuth, csrfToken: req.csrfToken() });
   } catch(err) {
     // Update this to render a 400 Error View
-    const error = err.message === DB_LOOKUP_ERR ? DB_LOOKUP_ERR : BAD_REQ_ERR;
-    res.render("authors/index", { error, isAuth });
+    const error = err.message === DB_LOOKUP_ERR ? "server-err" : "bad-request";
+    renderError(error, res, isAuth);
   }
 });
 
@@ -107,15 +90,16 @@ router.get("/:id", async (req: Request, res: Response) => {
 // @desc    Render form for editing author name
 // @access  Private
 router.get("/:id/edit", isAuthenticated, async (req: Request, res: Response) => {
+  const isAuth = req.user ? true : false;
   try {
     const author = await Author.findById(req.params.id).lean();
     res.render("authors/edit", {
       author,
-      isAuth: true,
+      isAuth,
       csrfToken: req.csrfToken()
     });
   } catch {
-    res.render("authors/index", { error: PAGE_ERR, isAuth: true });
+    renderError("server-err", res, isAuth);
   }
 });
 
@@ -123,16 +107,16 @@ router.get("/:id/edit", isAuthenticated, async (req: Request, res: Response) => 
 // @desc    Update an existing author entry
 // @access  Private
 router.put("/:id", isAuthenticated, authorFormChecker, async (req: Request, res: Response) => {
-  let author: IAuthor|null = null;
+  const isAuth = req.user ? true : false;
   try {
-    author = await Author.findById(req.params.id);
+    const author = await Author.findById(req.params.id);
     if (author == null) throw "Author not found";
     author.name = req.body.name;
     await author.save();
     res.redirect(`/authors/${req.params.id}`);
-  } catch {
-    const error = author != null ? "Failed to update author" : PAGE_ERR;
-    res.render("authors/index", { error, isAuth: true });
+  } catch(error) {
+    const type = error.message === "Author not found" ? "server-err" : "bad-request";
+    renderError(type, res, isAuth);
   }
 });
 
@@ -140,15 +124,14 @@ router.put("/:id", isAuthenticated, authorFormChecker, async (req: Request, res:
 // @desc    Remove an author from the database
 // @access  Private
 router.delete("/:id", isAuthenticated, async (req: Request, res: Response) => {
-  let author: IAuthor|null = null;
+  const isAuth = req.user ? true : false;
   try {
-    author = await Author.findById(req.params.id);
+    const author = await Author.findById(req.params.id);
     if (author == null) throw INVALID_AUTHOR_ERR;
     await author.remove();
     res.redirect("/authors");
-  } catch(err) {
-    const { message } = err;
-    const error = message !== AUTHOR_HAS_BOOKS_ERR ? "Failed to remove author" : INVALID_AUTHOR_ERR;
-    res.render("authors/index", { error, isAuth: true });
+  } catch(error) {
+    const type = error.message !== AUTHOR_HAS_BOOKS_ERR ? "server-err" : "bad-request";
+    renderError(type, res, isAuth);
   }
 });
